530e56761c6f8c7e617e3e38480a489758d0f17b
pavlin.marinov4@gmail.com <pavlin.marinov4@gmail.com>
2024-06-26 21:26:55 +0300

    delete task and keybinding fixed

--- delete_keybinding.php
+++ delete_keybinding.php
@@ -6,6 +6,9 @@ if (isset($_GET['id'])) {
     $keybinding_id = $_GET['id'];
     $user_id = $_SESSION['user_id']; // Assuming $_SESSION['user'] holds user information
 
+    $stmt_deleteHistory = $conn->prepare("DELETE FROM keybinding_version WHERE keybinding_id = ?");
+    $stmt_deleteHistory->bind_param("i",$keybinding_id);
+    $stmt_deleteHistory->execute();
     // Delete task from database
     $stmt = $conn->prepare("DELETE FROM keybindings WHERE id = ? AND user_id = ?");
     $stmt->bind_param("ii", $keybinding_id, $user_id);
--- delete_task.php
+++ delete_task.php
@@ -6,6 +6,10 @@ if (isset($_GET['id'])) {
     $task_id = $_GET['id'];
     $user_id = $_SESSION['user_id']; // Assuming $_SESSION['user'] holds user information
 
+    $stmt_deleteHistory = $conn->prepare("DELETE FROM task_version WHERE task_id = ?");
+    $stmt_deleteHistory->bind_param("i",$task_id);
+    $stmt_deleteHistory->execute();
+
     // Delete task from database
     $stmt = $conn->prepare("DELETE FROM tasks WHERE id = ? AND user_id = ?");
     $stmt->bind_param("ii", $task_id, $user_id);
